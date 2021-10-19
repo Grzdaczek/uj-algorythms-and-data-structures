8f20831740f763393f8627a8f0a12e087cdcc120
Grzdaczek <grzegorz.jnsk@gmail.com>
2021-10-19 23:42:21 +0200

    ONP przyjmuje teraz z std::cin

--- zestaw-01/ONP.cpp
+++ zestaw-01/ONP.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iterator>
+
 #include "LinkedStack.hpp"
 // #include "ArrayStack.hpp"
 
@@ -8,12 +13,17 @@ bool isnumber(std::string& s) {
 	return true;
 }
 
-int main(int argc, char* argv[]) {
+int main() {
 	Stack<char, 512> opstack;
+	std::vector<std::string> symbols;
 
-	for (int i=1; i<argc; i++) {
-		std::string symbol = argv[i];
-		
+	std::copy(
+		std::istream_iterator<std::string>(std::cin),
+		std::istream_iterator<std::string>(),
+		std::back_inserter(symbols)
+	);
+
+	for (std::string symbol : symbols) {
 		if (isnumber(symbol)) {
 			std::cout << symbol << " ";
 		}
@@ -21,8 +31,7 @@ int main(int argc, char* argv[]) {
 			opstack.push(symbol.at(0));
 		}
 		else if (symbol == ")") {
-			std::cout << opstack.pop();
-			if (!opstack.empty()) std::cout << " ";
+			std::cout << opstack.pop() << (opstack.empty() ? "\n" : " ");
 		}
 	}
 }
