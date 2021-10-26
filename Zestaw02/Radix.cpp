dea70b8f0a286482d218caa4ec31b5f8603a8367
Grzdaczek <grzegorz.jnsk@gmail.com>
2021-10-26 18:27:09 +0200

    Wczytywanie danych z stdin

--- Zestaw02/Radix.cpp
+++ Zestaw02/Radix.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include <vector>
 #include <functional>
 #include <cmath>
@@ -8,14 +9,14 @@
 template<typename T, size_t C, int N, int S = 256>
 void radix(
 	std::vector<T>& v,
-	std::function<int(const T&, int key)> category_fn
+	std::function<int(const T&, int key)> value_fn
 ) {
 	Queue<T, S> buckets[C];
 
 	for (int key = 0; key < N; key++) {
 		
 		for (auto& item : v) {
-			int i = category_fn(item, key);
+			int i = value_fn(item, key);
 			buckets[i].push(std::move(item));
 		}
 
@@ -32,17 +33,19 @@ void radix(
 
 int main() {
 
-	std::vector<int> v = {63, 19, 10, 84, 48, 81, 29, 16, 64, 57};
+	int n;
+	std::vector<int> v;
+	while (std::cin >> n)
+		v.push_back(n);
 
-	auto fn = [](const auto& element, auto key) {
+	auto value_fn = [](const auto& element, auto key) {
 		int position = std::pow(10, key);
 		int category = static_cast<int>(element / position) % 10;
 		return category;
 	};
 
-	radix<int, 10, 3>(v, fn);
-
-	std::cout << "[ ";
-	for(const auto& el : v) std::cout << el << " ";
-	std::cout << "]" << std::endl;
+	radix<int, 10, 9>(v, value_fn);
+	
+	for(const auto& i : v)
+		std::cout << i << std::endl;
 }
