e7a492413b294c4fa7071ef8de6aa417d109c5f9
Grzdaczek <grzegorz.jnsk@gmail.com>
2021-10-26 15:40:37 +0200

    Radix sort optymalizacja move

--- Zestaw02/Radix.cpp
+++ Zestaw02/Radix.cpp
@@ -2,38 +2,31 @@
 #include <vector>
 #include <functional>
 #include <cmath>
-#include "ArrayQueue.hpp"
-// #include "LinkedQueue.hpp"
+#include "LinkedQueue.hpp"
+// #include "ArrayQueue.hpp"
 
-template<typename T, int C, int N, int S = 256>
+template<typename T, size_t C, int N, int S = 256>
 void radix(
-	std::vector<T>& v, 
+	std::vector<T>& v,
 	std::function<int(const T&, int key)> category_fn
 ) {
 	Queue<T, S> buckets[C];
-	Queue<T, S> tmp;
-	for (auto el : v) tmp.push(el);
-	
-	for (int key = 0; key < N; key++) {
 
-		while (tmp.size() != 0) {
-			const T& el = tmp.top();
-			int category = category_fn(el, key);
-			buckets[category].push(el);
-			tmp.pop();
-		}
+	for (int key = 0; key < N; key++) {
 		
-		for (auto& bucket : buckets) {
-			while (bucket.size()) {
-				tmp.push(bucket.top());
-				bucket.pop();
+		for (auto& item : v) {
+			int i = category_fn(item, key);
+			buckets[i].push(std::move(item));
+		}
+
+		int i = 0;
+		for (auto& b : buckets) {
+			while (b.size()) {
+				v[i] = std::move(b.top());
+				b.pop();
+				i ++;
 			}
 		}
-	}
-
-	for (auto& el : v) {
-		el = tmp.top();
-		tmp.pop();
 	}
 }
 
@@ -41,11 +34,7 @@ int main() {
 
 	std::vector<int> v = {63, 19, 10, 84, 48, 81, 29, 16, 64, 57};
 
-	std::cout << "[";
-	for(const auto& el : v) std::cout << el << ",";
-	std::cout << "]" << std::endl;
-
-	auto fn = [](auto element, auto key) {
+	auto fn = [](const auto& element, auto key) {
 		int position = std::pow(10, key);
 		int category = static_cast<int>(element / position) % 10;
 		return category;
@@ -53,7 +42,7 @@ int main() {
 
 	radix<int, 10, 3>(v, fn);
 
-	std::cout << "[";
-	for(const auto& el : v) std::cout << el << ",";
+	std::cout << "[ ";
+	for(const auto& el : v) std::cout << el << " ";
 	std::cout << "]" << std::endl;
 }
