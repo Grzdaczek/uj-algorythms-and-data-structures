d516b88d7e016dc505e7051aef141343c1aee959
Grzdaczek <grzegorz.jnsk@gmail.com>
2021-10-28 20:52:19 +0200

    Radix warunek zakończenia

--- Zestaw02/Radix.cpp
+++ Zestaw02/Radix.cpp
@@ -6,14 +6,21 @@
 #include "LinkedQueue.hpp"
 // #include "ArrayQueue.hpp"
 
-template<typename T, size_t C, int N, int S = 256>
+template<typename T, size_t C, int S = 256>
 void radix(
 	std::vector<T>& v,
-	std::function<int(const T&, int key)> value_fn
+	std::function<int(const T&, int key)> value_fn,
+	std::function<int(const T&)> keys_fn
 ) {
 	Queue<T, S> buckets[C];
 
-	for (int key = 0; key < N; key++) {
+	auto max_keys = 0;
+	for (const auto& item : v) {
+		int x = keys_fn(item);
+		if (x > max_keys) max_keys = x;
+	}
+
+	for (int key = 0; key < max_keys; key++) {
 		
 		for (auto& item : v) {
 			int i = value_fn(item, key);
@@ -34,9 +41,12 @@ void radix(
 int main() {
 
 	int n;
+	int max = 0;
 	std::vector<int> v;
-	while (std::cin >> n)
+	while (std::cin >> n) {
 		v.push_back(n);
+		if (max > n) max = n;
+	}
 
 	auto value_fn = [](const auto& element, auto key) {
 		int position = std::pow(10, key);
@@ -44,7 +54,14 @@ int main() {
 		return category;
 	};
 
-	radix<int, 10, 9>(v, value_fn);
+	auto keys_fn = [](const auto& element) {
+		int keys = 1;
+		int el = element;
+		while ((el /= 10) > 0) keys ++;
+		return keys;
+	};
+
+	radix<int, 10>(v, value_fn, keys_fn);
 	
 	for(const auto& i : v)
 		std::cout << i << std::endl;
