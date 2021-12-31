df419f01be0d4d6032b02fa4b435ffbb0a06e376
Grzdaczek <grzegorz.jnsk@gmail.com>
2022-01-01 00:36:07 +0100

    Podstawowe operacje

--- Zestaw07/Dict.cpp
+++ Zestaw07/Dict.cpp
@@ -1,6 +1,14 @@
 #include "Dict.hpp"
 #include <iostream>
+#include <utility>
 
 int main(int argc, char* argv[]) {
+	auto dict = Dictionary<int, int>();
 
+	dict.set(0, 42);
+	dict.set(0, 20);
+	dict.set(1024, 11);
+
+	std::cout << dict.get(0) << std::endl;
+	std::cout << dict.get(1024) << std::endl;
 }
--- Zestaw07/Dict.hpp
+++ Zestaw07/Dict.hpp
@@ -1,48 +1,129 @@
 #include <utility>
 #include <vector>
-
-// template<typename K, typename V>
-// class Dictionary;
-
-// template<typename K, typename V>
-// class EntryProxy {
-// public:
-//     using Entry = std::pair<K, V>;
-
-// private:
-//     Dictionary* mDict;
-// };
+#include <algorithm>
+#include <cstring>
+#include <iostream>
 
 template<typename K, typename V>
 class Dictionary {
 public:
-    using Entry = std::pair<K, V>;
-    using Bucket = std::vector<Entry>;
+	using Entry = std::pair<K, V>;
+	using Bucket = std::vector<Entry>;
 
 private:
-    Bucket* mBuckets;
-    size_t mSize;
-    size_t mBitCapacity;
-    size_t mRawCapacity;
+	size_t mSize;
+	size_t mBitCapacity;
+	Bucket* mBuckets;
 
-    void mGrow(size_t newCapacity);
-    void mShrink(size_t newCapacity);
-    size_t hash(char[sizeof(V)]);
+	static const size_t DEFAULT_BIT_CAPACITY = 6;
+
+	void mGrow(size_t newBitCapacity);
+
+	void mShrink(size_t newBitCapacity);
+
+	size_t mHash(const K& k) const {
+		const size_t BLOCK_SIZE = 32;
+		uint8_t data[BLOCK_SIZE]; 
+		std::memcpy(data, &k, std::min(sizeof(K), BLOCK_SIZE));
+
+		// TODO: hash data
+		return k % mCapacity();
+	}
+
+	constexpr size_t mCapacity() const {
+		return 1 << mBitCapacity;
+	}
+
+	constexpr float mLoadFactor() const {
+		return (float) mSize / (float) mCapacity();
+	};
 
 public:
-    Dictionary();
-    Dictionary(const Dictionary& other);
-    Dictionary(Dictionary&& other);
-    
-    void clear();                    // Czyści słownik
-    void set(const Entry& p);        // Dodaje parę klucz-wartość do słownika
-    void set(Entry&& p);             // Dodaje parę klucz-wartość do słownika
-    V& get(const K& k);              // Zwraca wartość dla klucza
-    V& operator[](const K& k);       // Zwraca wartość dla klucza
-    bool has(const K& k);            // Sprawdza czy słownik zawiera klucz
-    bool erase(const K& k);          // Usuwa parę od danym kluczu
-    size_t size();                   // Zwraca liczbę par
-    bool empty();                    // Sprawdza czy słownik jest pusty
-    
-    void buckets();
+	Dictionary()
+		: mSize(0)
+		, mBitCapacity(DEFAULT_BIT_CAPACITY)
+		, mBuckets(new Bucket[mCapacity()])
+	{}
+
+	Dictionary(const Dictionary& other)
+		: mSize(other.mSize)
+		, mBitCapacity(other.mBitCapacity)
+		, mBuckets(new Bucket[mCapacity()])
+	{
+		for (int i = 0; i < mCapacity(); ++i)
+			mBuckets[i] = other.mBuckets[i];
+	}
+
+	Dictionary(Dictionary&& other)
+		: mSize(other.mSize)
+		, mBitCapacity(other.mBitCapacity)
+		, mBuckets(other.mBuckets)
+	{
+		other.mBuckets = nullptr;
+	}
+
+	~Dictionary() {
+		delete[] mBuckets;
+	}
+	
+	void clear() {
+		for (int i = 0; i < mCapacity(); ++i)
+			mBuckets[i].clear();
+		
+		mSize = 0;
+	}
+
+	template<typename P, typename Q>
+	void set(P&& k, Q&& v) {
+		int index = mHash(k) & (mBitCapacity - 1);
+		Bucket& b = mBuckets[index];
+		
+		if(has(k))
+			erase(k);
+
+		b.push_back(Entry(std::forward<P>(k), std::forward<Q>(v)));
+		// TODO: implement
+	}
+
+	const V& get(const K& k) const {
+		int index = mHash(k) & (mBitCapacity - 1);
+		Bucket& b = mBuckets[index];
+		
+		for (auto it = b.begin(); it != b.end(); ++it)
+			if (it->first == k)
+				return it->second;
+		
+		throw std::out_of_range("No entry f0r given key");
+	}
+
+	// V& operator[](const K& k);    // Zwraca wartość dla klucza
+
+	bool has(const K& k) const {
+		int index = mHash(k) & (mBitCapacity - 1);
+
+		for (const Entry e : mBuckets[index])
+			if (e.first == k)
+				return true;
+
+		return false;
+	};
+
+	bool erase(const K& k) {
+		int index = mHash(k) & (mBitCapacity - 1);
+		Bucket& b = mBuckets[index];
+
+		for (auto it = b.begin(); it != b.end(); ++it)
+			if (it->first == k) {
+				b.erase(it);
+				mSize -= 1;
+				return true;
+			}
+
+		return false;
+	};
+
+	size_t size() const;             // Zwraca liczbę par
+	bool empty() const;              // Sprawdza czy słownik jest pusty
+	
+	void buckets() const;
 };
